b29558e5de4f44d6b9f4de4c2e2330a4c872fc33
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-27 10:37:07 +0200

    Serializing Relationship on collection field using Hyperlink

--- store/views.py
+++ store/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset, many=True, context={ 'request': request })
     return Response(serializer.data)
  
 @api_view()
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    collection = Collection.objects.get(pk=pk)
+    serializer = CollectionSerializer(collection)
     return Response(serializer.data)
