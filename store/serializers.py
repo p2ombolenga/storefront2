b138310c8001b25305ee32443bbbaa01dcd99e96
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-27 17:33:02 +0200

    Saving: create and update objects

--- store/serializers.py
+++ store/serializers.py
@@ -10,7 +10,7 @@ class CollectionSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'unit_price', 'inventory', 'price_with_tax', 'collection']
         
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
--- store/views.py
+++ store/views.py
@@ -15,14 +15,23 @@ def product_list(request):
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        return Response('Ok')
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
  
-@api_view()
+@api_view(['GET', 'PUT'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product) 
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product) 
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 @api_view()
 def collection_detail(request, pk):
