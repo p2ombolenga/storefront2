a3b4312764bd479c4894ca534a22241f981f16a2
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-29 12:38:13 +0200

    Creating Reviews API

--- store/models.py
+++ store/models.py
@@ -92,8 +92,15 @@ class CartItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     zip = models.CharField(max_length=255, default='00000')
-    city = models.CharField(max_length=255)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) 
+    city = models.CharField(max_length=255) 
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
 
 
 
--- store/views.py
+++ store/views.py
@@ -6,8 +6,8 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from .models import Product, Collection, OrderItem
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Product, Collection, OrderItem, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -16,7 +16,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     
     def get_serializer_context(self):
-        return {'request': self.request}
+        return {'request': self.requmodelsest}
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
@@ -29,7 +29,7 @@ class ProductViewSet(ModelViewSet):
 class CollectionViewSet(ModelViewSet):
 
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
-    serializer_class = CollectionSerializer
+    serializer_class = CollectionSerializer  
 
     def destroy(self, request, *args, **kwargs):
           if Product.objects.filter(collection=kwargs['pk']).count() > 0:
@@ -38,3 +38,8 @@ class CollectionViewSet(ModelViewSet):
                 status=status.HTTP_405_METHOD_NOT_ALLOWED
                 )
           return super().destroy(request, *args, **kwargs)
+    
+
+class ReviewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
