b21b87451d780b5e3d27a3999aba25a66744d6eb
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-05-06 19:33:47 +0200

    Applying permissions

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, AllowAny 
 from rest_framework.filters import SearchFilter, OrderingFilter 
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer
@@ -89,7 +90,13 @@ class CartItemViewSet(ModelViewSet):
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
     @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request):
         (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
